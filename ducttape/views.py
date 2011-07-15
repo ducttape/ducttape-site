5dce3daa5684e727af5138640ec4b2687438c74b
opatut <opatutlol@aol.com>
2011-07-15 23:39:14 +0200

    Added pretty much all static pages

--- ducttape/views.py
+++ ducttape/views.py
@@ -16,3 +16,27 @@ def blog_post(id):
     post = BlogPost.query.filter_by(id = id).first_or_404()
     return render_template("blog_post.html", post = post)
 
+@app.route("/engine/")
+def engine():
+    return render_template("engine.html")
+
+@app.route("/editor/")
+def editor():
+    return render_template("editor.html")
+
+@app.route("/tutorials/")
+def tutorials():
+    return render_template("tutorials.html")
+
+@app.route("/download/")
+def download():
+    return render_template("download.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/donate/")
+def donate():
+    return render_template("donate.html")
+
