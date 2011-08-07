286d18b30d06ea27d4136aaef88af219d0dcfc9e
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-08-07 14:24:27 +0200

    Adding thanks page

--- ducttape/views.py
+++ ducttape/views.py
@@ -45,3 +45,6 @@ def contact():
 def donate():
     return render_template("donate.html")
 
+@app.route("/thanks/", methods=["GET", "POST"])
+def thanks():
+    return render_template("thanks.html")
